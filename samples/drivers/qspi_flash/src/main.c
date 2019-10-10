05b1adf7d795b66c2887e330d8de9783365c2599
kamil.lazowski@nordicsemi.no <kamil.lazowski@nordicsemi.com>
2019-10-10 13:43:15 +0200

    Work in progress on DTS

--- samples/drivers/qspi_flash/src/main.c
+++ samples/drivers/qspi_flash/src/main.c
@@ -11,22 +11,12 @@
 #include <zephyr.h>
 #include <device.h>
 #include <gpio.h>
-//#include <drivers/qspi.h>
+#include <drivers/qspi.h>
 #include <logging/log.h>
-#include <fs/fs.h>
-#include <fs/littlefs.h>
-#include <storage/flash_map.h>
+
 
 /* Matches LFS_NAME_MAX */
 #define MAX_PATH_LEN 255
-
-FS_LITTLEFS_DECLARE_DEFAULT_CONFIG(storage);
-static struct fs_mount_t lfs_storage_mnt = {
-	.type = FS_LITTLEFS,
-	.fs_data = &storage,
-	.storage_dev = (void *)DT_FLASH_AREA_STORAGE_ID,
-	.mnt_point = "/lfs",
-};
 
 LOG_MODULE_REGISTER(qspi_flash, LOG_LEVEL_DBG);
 
@@ -53,11 +43,11 @@ enum{
 uint8_t chip_mem[TEST_MAX_MEM]={0};	// Holds data read from FLASH memory
 uint8_t test[TEST_MAX_MEM]={0};		// Holds data that will be sent to FLASH memory
 
-//struct device *qspi;				// Defines pointer to qspi device
-//struct qspi_config qspi_cfg;		// Config for qspi device
+struct device *qspi;				// Defines pointer to qspi device
+struct qspi_config qspi_cfg;		// Config for qspi device
 
 /* Private function prototypes -----------------------------------------------*/
-//static void qspi_configure(struct qspi_config * pCfg);
+static void qspi_configure(struct qspi_config * pCfg);
 //static void qspi_configure(void);
 //void qspi_init(uint32_t frequency, uint8_t addressMode, uint8_t dataLines);
 
@@ -72,142 +62,24 @@ void main(void)
 #if defined(TCA2_INTEGRATION)
 	tca_init(&g_target_comm, &TARGET_COMM_DEFAULT_CONFIG);
 #endif
-	struct fs_mount_t *mp = &lfs_storage_mnt;
-	unsigned int id = (uintptr_t)mp->storage_dev;
-	char fname[MAX_PATH_LEN];
-	struct fs_statvfs sbuf;
-	const struct flash_area *pfa;
-	int rc;
-
-	snprintf(fname, sizeof(fname), "%s/boot_count", mp->mnt_point);
-
-	rc = flash_area_open(id, &pfa);
-	if (rc < 0) {
-		printk("FAIL: unable to find flash area %u: %d\n",
-		       id, rc);
-		return;
-	}
-
-	printk("Area %u at 0x%x on %s for %u bytes\n",
-	       id, (unsigned int)pfa->fa_off, pfa->fa_dev_name,
-	       (unsigned int)pfa->fa_size);
-
-	/* Optional wipe flash contents */
-	if (IS_ENABLED(CONFIG_APP_WIPE_STORAGE)) {
-		printk("Erasing flash area ... ");
-		rc = flash_area_erase(pfa, 0, pfa->fa_size);
-		printk("%d\n", rc);
-		flash_area_close(pfa);
-	}
-
-	rc = fs_mount(mp);
-	if (rc < 0) {
-		printk("FAIL: mount id %u at %s: %d\n",
-		       (unsigned int)mp->storage_dev, mp->mnt_point,
-		       rc);
-		return;
-	}
-	printk("%s mount: %d\n", mp->mnt_point, rc);
-
-	rc = fs_statvfs(mp->mnt_point, &sbuf);
-	if (rc < 0) {
-		printk("FAIL: statvfs: %d\n", rc);
-		goto out;
-	}
-
-	printk("%s: bsize = %lu ; frsize = %lu ;"
-	       " blocks = %lu ; bfree = %lu\n",
-	       mp->mnt_point,
-	       sbuf.f_bsize, sbuf.f_frsize,
-	       sbuf.f_blocks, sbuf.f_bfree);
-
-	struct fs_dirent dirent;
-
-	rc = fs_stat(fname, &dirent);
-	printk("%s stat: %d\n", fname, rc);
-	if (rc >= 0) {
-		printk("\tfn '%s' siz %u\n", dirent.name, dirent.size);
-	}
-
-	struct fs_file_t file;
-
-	rc = fs_open(&file, fname);
-	if (rc < 0) {
-		printk("FAIL: open %s: %d\n", fname, rc);
-		goto out;
-	}
-
-	u32_t boot_count = 0;
-
-	if (rc >= 0) {
-		rc = fs_read(&file, &boot_count, sizeof(boot_count));
-		printk("%s read count %u: %d\n", fname, boot_count, rc);
-		rc = fs_seek(&file, 0, FS_SEEK_SET);
-		printk("%s seek start: %d\n", fname, rc);
-
-	}
-
-	boot_count += 1;
-	rc = fs_write(&file, &boot_count, sizeof(boot_count));
-	printk("%s write new boot count %u: %d\n", fname,
-	       boot_count, rc);
-
-	rc = fs_close(&file);
-	printk("%s close: %d\n", fname, rc);
-
-	struct fs_dir_t dir = { 0 };
-
-	rc = fs_opendir(&dir, mp->mnt_point);
-	printk("%s opendir: %d\n", mp->mnt_point, rc);
-
-	while (rc >= 0) {
-		struct fs_dirent ent = { 0 };
-
-		rc = fs_readdir(&dir, &ent);
-		if (rc < 0) {
-			break;
-		}
-		if (ent.name[0] == 0) {
-			printk("End of files\n");
-			break;
-		}
-		printk("  %c %u %s\n",
-		       (ent.type == FS_DIR_ENTRY_FILE) ? 'F' : 'D',
-		       ent.size,
-		       ent.name);
-	}
-
-	(void)fs_closedir(&dir);
-
-out:
-	rc = fs_unmount(mp);
-	printk("%s unmount: %d\n", mp->mnt_point, rc);
-
-
-
-
-
-
-
-
 
 	/* Get binding */
-//	qspi = device_get_binding(DT_NORDIC_NRF_QSPI_QSPI_0_LABEL);
+	qspi = device_get_binding(DT_NORDIC_NRF_QSPI_QSPI_0_LABEL);
 
 //	/* Check acquired pointer */
-//	if (!qspi) {
-//		LOG_INF("QSPI: Device driver not found.");
-//	}
-//	else{
-//		LOG_INF("QSPI: Device driver found.");
-//	}
+	if (!qspi) {
+		LOG_INF("QSPI: Device driver not found.");
+	}
+	else{
+		LOG_INF("QSPI: Device driver found.");
+	}
 
 
 
 
 
 	/* Assign config */
-//	qspi_configure(&qspi_cfg);
+	qspi_configure(&qspi_cfg);
 //	qspi_configure();
 //	qspi_init(8000000,QSPI_ADDRESS_MODE_24BIT, QSPI_DATA_LINES_QUAD);
 //
@@ -234,12 +106,12 @@ out:
 // * @param pCfg	- pointer to the qspi structure
 // * @retval None
 // */
-////static void qspi_configure(struct qspi_config * pCfg){
-////	pCfg->frequency = 8000000;
-////	pCfg->operation = (	QSPI_CS_DELAY_SET(8) 							|
-////						QSPI_DATA_LINES_SET(QSPI_DATA_LINES_QUAD)		|
-////						QSPI_ADDRESS_MODE_SET(QSPI_ADDRESS_MODE_24BIT));
-////}
+static void qspi_configure(struct qspi_config * pCfg){
+	pCfg->frequency = 8000000;
+	pCfg->operation = (	QSPI_CS_DELAY_SET(8) 							|
+						QSPI_DATA_LINES_SET(QSPI_DATA_LINES_QUAD)		|
+						QSPI_ADDRESS_MODE_SET(QSPI_ADDRESS_MODE_24BIT));
+}
 //static void qspi_configure(void){
 //	qspi_cfg.frequency = 8000000;
 //	qspi_cfg.operation = (	QSPI_CS_DELAY_SET(8) 							|
